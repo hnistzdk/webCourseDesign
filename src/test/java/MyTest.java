e7dbe31ba4e92e8f2a22d0c55e85d8b3e34ffadb
hnist <369365576@qq.com>
2021-12-12 17:29:03 +0800

    测试日志

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,18 @@
+import com.mysql.cj.log.LogFactory;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author zdk
+ * @date 2021/12/12 17:25
+ */
+public class MyTest {
+    private final Logger log = LoggerFactory.getLogger(MyTest.class);
+
+    @Test
+    public void test() {
+        log.debug("测试-> {}", "abc");
+        log.info("测试-> {}", "123");
+    }
+}
